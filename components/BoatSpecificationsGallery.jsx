d41433b04e75d05dc802e272de9cf950fc3f8153
Kex027 <adafisz27@gmail.com>
2024-05-01 13:03:04 +0200

    Added gallery to boat specifications

--- components/BoatSpecificationsGallery.jsx
+++ components/BoatSpecificationsGallery.jsx
@@ -1,9 +1,27 @@
 import { Stack } from "@mui/joy";
 import style from "../styles/boatSpecificationsGallery.module.css";
 import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import "yet-another-react-lightbox/styles.css";
+import { Lightbox } from "yet-another-react-lightbox";
 
 const BoatSpecificationsGallery = () => {
   const { t } = useTranslation();
+  const [openImageModal, setOpenImageModal] = useState(false);
+  const [imageIndex, setImageIndex] = useState(0);
+  const images = [
+    { src: "mainPageYacht.jpg" },
+    { src: "mainPageYacht.jpg" },
+    { src: "mainPageYacht.jpg" },
+    { src: "mainPageYacht.jpg" },
+    { src: "mainPageYacht.jpg" },
+    { src: "mainPageYacht.jpg" },
+    { src: "mainPageYacht.jpg" },
+    { src: "mainPageYacht.jpg" },
+  ];
+  const closeModal = () => setOpenImageModal(false);
+  const openModal = () => setOpenImageModal(true);
 
   return (
     <Stack
@@ -19,7 +37,58 @@ const BoatSpecificationsGallery = () => {
       >
         <h1>{t("Gallery")}</h1>
         <h2>{t("See our boat")}</h2>
+
+        <Stack
+          direction={"row"}
+          flexWrap={"wrap"}
+          gap={2}
+          justifyContent={"center"}
+          className={style.gallery}
+        >
+          {images?.map(({ src }, index) => (
+            <motion.img
+              initial={{
+                opacity: 0,
+                filter: "brightness(1)",
+                transform: "scale(1)",
+              }}
+              whileInView={{ opacity: 1 }}
+              whileHover={{
+                filter: "brightness(.5)",
+                transform: "scale(1.05)",
+                cursor: "pointer",
+              }}
+              whileTap={{ filter: "brightness(1)", transform: "scale(.9)" }}
+              src={src}
+              alt="Yacht"
+              key={index}
+              className={style.img}
+              onClick={() => {
+                openModal();
+                setImageIndex(index);
+              }}
+            />
+          ))}
+        </Stack>
       </Stack>
+
+      {openImageModal && (
+        <Lightbox
+          styles={{
+            container: {
+              backgroundColor: "rgba(0, 0, 0, .75)",
+              backdropFilter: "blur(3px)",
+            },
+          }}
+          open={openImageModal}
+          index={imageIndex}
+          close={closeModal}
+          slides={images}
+          toolbar={{
+            buttons: ["close"],
+          }}
+        />
+      )}
     </Stack>
   );
 };
